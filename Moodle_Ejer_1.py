f7bd3a6e07fb3968742d6c217823e3ea60973fe1
Santiago2245234 <conder0899@gmail.com>
2026-09-07 15:44:11 -0500

    Add files via upload

---
+++ Moodle_Ejer_1.py
@@ -0,0 +1,10 @@
+nombre = input("Ingrese su nombre: ")
+apellido = input("Ingrese su apellido: ")
+edad = int(input("Ingrese su edad: "))
+direccion = input("Ingrese su dirección: ")
+celular = input("Ingrese su número de teléfono: ")
+genero = input("Ingrese su género: ")
+
+print(f"Su nombre es: {nombre}, apellido: {apellido}, "
+      f"edad: {edad}, dirección: {direccion}, " 
+      f"celular: {celular}, género: {genero}")
